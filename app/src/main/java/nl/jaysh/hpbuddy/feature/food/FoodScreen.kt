44c7243a747f5fe6fa5e33be02aaded984cd2219
Jay <sanjaysheombar@gmail.com>
2024-10-19 02:11:14 +0200

    Update FoodScreen.kt

--- app/src/main/java/nl/jaysh/hpbuddy/feature/food/FoodScreen.kt
+++ app/src/main/java/nl/jaysh/hpbuddy/feature/food/FoodScreen.kt
@@ -1,14 +1,15 @@
 package nl.jaysh.hpbuddy.feature.food
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import nl.jaysh.hpbuddy.core.designsystem.HpbuddyTheme
@@ -26,11 +27,12 @@ private fun FoodScreenContent(
     onEvent: (FoodViewModelEvent) -> Unit,
 ) {
     Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        content = { Text(text = "Food") },
-    )
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp),
+    ) {
+        Text(text = "Food", style = MaterialTheme.typography.displayLarge)
+    }
 }
 
 // PREVIEWS
