240a9f7ac82c44b8f4c115bdf350b140378f06f0
Jay <sanjaysheombar@gmail.com>
2024-10-19 02:11:10 +0200

    Created DataSource module to switch between inMemory or Impl DataSource

---
+++ app/src/main/java/nl/jaysh/hpbuddy/di/DataSourceModule.kt
@@ -0,0 +1,52 @@
+package nl.jaysh.hpbuddy.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import nl.jaysh.hpbuddy.BuildConfig
+import nl.jaysh.hpbuddy.core.data.local.room.datasource.food.InMemoryFoodDataSource
+import nl.jaysh.hpbuddy.core.data.local.room.datasource.food.LocalFoodDataSource
+import nl.jaysh.hpbuddy.core.data.local.room.datasource.food.LocalFoodDataSourceImpl
+import nl.jaysh.hpbuddy.core.data.local.room.datasource.person.InMemoryPersonDataSource
+import nl.jaysh.hpbuddy.core.data.local.room.datasource.person.LocalPersonDataSource
+import nl.jaysh.hpbuddy.core.data.local.room.datasource.person.LocalPersonDataSourceImpl
+import nl.jaysh.hpbuddy.core.data.local.room.datasource.weight.InMemoryWeightDataSource
+import nl.jaysh.hpbuddy.core.data.local.room.datasource.weight.LocalWeightDataSource
+import nl.jaysh.hpbuddy.core.data.local.room.datasource.weight.LocalWeightDataSourceImpl
+import javax.inject.Singleton
+
+@Suppress("KotlinConstantConditions")
+@Module
+@InstallIn(SingletonComponent::class)
+object DataSourceModule {
+    @Provides
+    @Singleton
+    fun providesLocalFoodDataSource(): LocalFoodDataSource {
+        return if (BuildConfig.FLAVOR == "inMemory") {
+            InMemoryFoodDataSource()
+        } else {
+            LocalFoodDataSourceImpl()
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun providesLocalPersonDataSource(): LocalPersonDataSource {
+        return if (BuildConfig.FLAVOR == "inMemory") {
+            InMemoryPersonDataSource()
+        } else {
+            LocalPersonDataSourceImpl()
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun providesLocalWeightDataSource(): LocalWeightDataSource {
+        return if (BuildConfig.FLAVOR == "inMemory") {
+            InMemoryWeightDataSource()
+        } else {
+            LocalWeightDataSourceImpl()
+        }
+    }
+}
